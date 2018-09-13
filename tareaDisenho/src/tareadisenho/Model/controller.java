da23db2bfd4b91f5065714f035da4574f91461c3
Unknown <luismorah77@gmail.com>
2018-09-12 19:24:04 -0600

    metodos del controller

--- tareaDisenho/src/tareadisenho/Model/controller.java
+++ tareaDisenho/src/tareadisenho/Model/controller.java
@@ -3,7 +3,10 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package tareadisenho;
+package tareadisenho.Model;
+
+import java.util.Collection;
+import tareadisenho.Controller.DTO;
 
 /**
  *
@@ -22,7 +25,38 @@ public class controller {
     
     public void predefinirAlfabeto(){
         setAlfabeto("abcdefghijklmnopqrstuvwxyz");
+        System.out.println("alfabeto predefinido con exito");
     }
     
+    public void cargarAlfabetos(){
+        System.out.println("alfabetos cargados exitosamente");
+    }
+    
+    public void procesarPeticion(DTO datos){
+        
+        
+    
+    }
+    
+    public void agregarAlfabeto(DTO datos){}
+    public void actualizarAlfabeto(DTO datos){}
+    
+    public void validar(DTO datos){
+    
+    }
+    
+    public void CargarAlgoritmos(){
+        System.out.println("algoritmos cargados exitosamente");
+    }
+    
+    public void ejecutarAlgoritmos(){
+        System.out.println("ejecutando algoritmo(s) requerido(s)");
+    }
+    
+    public void escribir(DTO datos){
+    TXT escritor = new TXT();
+    escritor.escribir(datos);
+    System.out.println("txt escrito exitosamente");
+    }
     
 }
